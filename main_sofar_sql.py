df6d5dd9a1669c436b2ce1f682a6a0135c621892
benchmarex <120053921+benchmarex@users.noreply.github.com>
2023-03-13 21:17:42 +0100

    add sql service

--- main_sofar_sql.py
+++ main_sofar_sql.py
@@ -1,11 +1,8 @@
 """
-Author Marek Pikulski 12.03.2023.
-benchmarek[at]outlook[dot]com
-https://github.com/benchmarex
+Author Marek Pikulski 12.03.2023. benchmarek[at]outlook[dot]com https://github.com/benchmarex
 
-It is a project that downloads data on the production and operating parameters of the photovoltaic system sent from the
-SofarSolar inverter using local connection MODBUS TCP/IP.
-The data is sent to the local mysql server.
+This project downloads data solar production and operating parameters of the photovoltaic system from the SofarSolar
+inverter via local connection MODBUS TCP/IP. The data is sent to the local mysql server.
 The script is written in Python and invoked using the cron mechanism at intervals of __ minutes.
 """
 
